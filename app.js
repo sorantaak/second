dec16151d3bd768316e99e88a90ff5b0b86fbef2
soran <soran.taak@gmail.com>
2023-08-15 18:04:21 +0330

    number methods

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-let isVlidPassword = function (password) {
-	if (password.length > 8 && password.includes("12345")) {
-		return true;
-	} else {
-		return false;
-	}
-};
-console.log(isVlidPassword("sorantaak12345"));
-console.log(isVlidPassword("sor1234dad"));
-console.log(isVlidPassword("sorantaak12345"));
+let num = 65.1;
+let res = num.toFixed(5);
+// console.log(res);
+
+// console.log(Math.round(num))
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+let num1 = 100;
+let num2 = 200;
+let randomBetweenTwoNum = num1 + Math.round(Math.random() * (num2 - num1));
+console.log(randomBetweenTwoNum);
