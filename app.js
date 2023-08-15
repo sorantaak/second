24a9a4e6df364a2dfa092f8434e31da03f56a6f0
soran <soran.taak@gmail.com>
2023-08-15 10:34:42 +0330

    variable string and number

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+// string
+let firstName = "Soran"; // camel case rule
+let lastName = "Taak";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Number
+let num1 = 12;
+let num2 = 20;
+let sum = num1 + num2;
+
+let res = fullName + num1 + num2;
+console.log(res);
+console.log(sum);
