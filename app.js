ac4c05af3cdef679c927035e18533c0075ca0011
soran <soran.taak@gmail.com>
2023-08-15 11:36:10 +0330

    template string

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-let sum = function (num1, num2, num3) {
-	let res = num1 + num2 + num3;
-	return res;
-};
-
-let showResult = sum(10, 20, 69.65);
-console.log(showResult);
-
 let getUserInfo = function (name = "Test", id = 2) {
-	return `name: ${name} Id:${id}`;
+	return `name: ${name} Id:${id}`; // use template string in this line
 };
 console.log(getUserInfo("Soran", 50));
 console.log(getUserInfo());
