4a6ec1159c869644e222d9679631d620d8d2127d
soran <soran.taak@gmail.com>
2023-08-15 17:47:12 +0330

    challenge for password check

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-let fullName = "soran taak";
-console.log(fullName.length);
-console.log(fullName.toLocaleLowerCase());
-console.log(fullName.toUpperCase());
-
-let password = "SoranTaak123456"
-console.log(password.includes('1234'))
-
-let email = " soran.taak@gmail.com ";
-console.log(email);
-console.log(email.trim());
+let isVlidPassword = function (password) {
+	if (password.length > 8 && password.includes("12345")) {
+		return true;
+	} else {
+		return false;
+	}
+};
+console.log(isVlidPassword("sorantaak12345"));
+console.log(isVlidPassword("sor1234dad"));
+console.log(isVlidPassword("sorantaak12345"));
