e1749a5406d857914e96b2074b4450f6bead3039
soran <soran.taak@gmail.com>
2023-08-15 11:51:18 +0330

    Object

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-let getUserInfo = function (name = "Test", id = 2) {
-	return `name: ${name} Id:${id}`; // use template string in this line
+let userInfo = {
+	userId: 98,
+	username: "SoranTaak",
+	role: "admin",
 };
-console.log(getUserInfo("Soran", 50));
-console.log(getUserInfo());
+console.log(`User role is ${userInfo.role}`);
+
+// another test
+let userInfoOne = {
+	userId: 32,
+	username: "SaharMohammadAmini",
+	role: "author",
+};
+let userInfoTwo = {
+	userId: 74,
+	username: "AsrinTaak",
+	role: "user",
+};
+
+let fetchUserData = function (user) {
+	console.log(`User nam is: ${user.username}`);
+};
+
+fetchUserData(userInfoOne);
+fetchUserData(userInfoTwo);
