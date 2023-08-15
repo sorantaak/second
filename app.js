8d310626388780e2ef59130ad31391480aae45fd
soran <soran.taak@gmail.com>
2023-08-15 11:01:51 +0330

    variable scopr

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-let validEmail = true;
-let validPassword = false;
-
-if (validEmail && validPassword) {
-	console.log("logged in");
-} else if (validEmail || validPassword) {
-	console.log("Is correct!");
+let firstName = "soran";
+if (true) {
+	let lastName = "taak";
+	console.log(lastName);
+	if (true) {
+		let age = 30;
+		console.log(age);
+	}
 }
 
-let isUser = true;
-let userRole = "admin";
-if (isUser) {
-	console.log("Welcome user");
-} else if (userRole === "admin") {
-	console.log("Welcome admin");
-} else {
-	console.log("welcome");
+console.log(firstName);
+
+///////// second check blocking
+let name1 = "Test1";
+
+if (true) {
+	let name1 = "Test2";
+	
+	if (true) {
+		let name1 = "Test3";
+		console.log(name1);
+	}
 }
