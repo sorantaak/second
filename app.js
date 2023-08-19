12ae4e285d388ce9f7b087be395662d294b8bcf0
soran <soran.taak@gmail.com>
2023-08-19 09:49:19 +0330

    challenge for array

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const cartItems = ["Book1", "Book2", "Book3", "Book4"];
 
+cartItems.shift();
+cartItems.splice(1, 1);
+cartItems.push("New Book");
+console.log(cartItems);
 cartItems.forEach(function (item, index) {
-	console.log(`index[${index}] = ${item}`);
+	console.log(`index[${index + 1}]-Product name :${item}`);
 });
