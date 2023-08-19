4e6e233a851b13c2d27fdfba6934d344e86be1d4
soran <soran.taak@gmail.com>
2023-08-19 09:36:08 +0330

    array methods

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const tasks = ["Task1", "Task2", "Task3", 2, true];
-console.log(tasks);
-console.log(tasks.length);
-console.log(tasks[1]);
+const cartItems = ["Book1", "Book2", "Book3", "Book4"];
 
-const cartItems = ["Book1", "Book2", "Book3"];
-let res = `You have ${cartItems.length} products in your cart`;
+cartItems.push("Book5"); // Add to the first of the array
+console.log(cartItems);
+
+const removeLastItem = cartItems.pop(); // Delete from the end of the array and return removed item
+console.log(removeLastItem);
+console.log(cartItems);
+
+cartItems.unshift("Book0");
+console.log(cartItems);
+
+cartItems.shift()
+console.log(cartItems);
+
+const res= cartItems.splice(1,2,'new item','new items',2)
+console.log(cartItems);
 console.log(res);
-
-console.log(
-	`your first item is ${cartItems[0]} and last item is ${
-		cartItems[cartItems.length - 1]
-	}`
-);
