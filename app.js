62357a119c6fd2d9c7f81653beaedb3960ab0b45
soran <soran.taak@gmail.com>
2023-08-19 09:21:31 +0330

    array in javascript

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let isUser = true;
-isUser = false;
-console.log(isUser);
-const userInfo = {
-	id: 2,
-	name: "soran taak",
-};
-// userInfo = {
-// 	id: 45,
-// 	name: "Alie",
-// };
-userInfo.id = 20;
+const tasks = ["Task1", "Task2", "Task3", 2, true];
+console.log(tasks);
+console.log(tasks.length);
+console.log(tasks[1]);
 
-console.log(userInfo);
+const cartItems = ["Book1", "Book2", "Book3"];
+let res = `You have ${cartItems.length} products in your cart`;
+console.log(res);
+
+console.log(
+	`your first item is ${cartItems[0]} and last item is ${
+		cartItems[cartItems.length - 1]
+	}`
+);
